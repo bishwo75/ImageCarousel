97c8a6e40a888bef0f3b38689bd592d607c2591b
bishwo75 <54118149+bishwo75@users.noreply.github.com>
2023-01-13 21:19:44 -0600

    Update faq.js

--- faq.js
+++ faq.js
@@ -39,3 +39,45 @@ const questions = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
   }
 ];
+
+
+// //gENERATING Quotes example:
+// import "./styles.css";
+// import { useState, useEffect } from "react";
+
+// function getRandomQuote(quotes) {
+//   return quotes[Math.floor(Math.random() * quotes.length)];
+// }
+
+// export default function App() {
+//   const [quotes, setQuotes] = useState([]);
+//   const [quote, setQuote] = useState(null);
+
+//   useEffect(() => {
+//     fetch("https://type.fit/api/quotes")
+//       .then((res) => res.json())
+//       .then((json) => {
+//         setQuotes(json);
+//         setQuote(json[0]);
+//       });
+//   }, []);
+
+//   function getNewQuote() {
+//     setQuote(getRandomQuote(quotes));
+//   }
+
+//   return (
+//     <main>
+//       <h1>Project 3: Quote Generator</h1>
+//       <section>
+//         <button onClick={getNewQuote}>New Quote</button>
+//         <h3>
+//           <span>“</span>
+//           {quote?.text}
+//         </h3>
+//         <i>- {quote?.author}</i>
+//       </section>
+//     </main>
+//   );
+// }
+
